b1192672c148864cb19ee2f2c48c0c15d4052ccb
Lwinmink <lwinmin665@gmail.com>
2020-10-06 16:37:46 +0630

    First Upload

---
+++ addstudent.php
@@ -0,0 +1,44 @@
+<?php 
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$gender = $_POST['gender'];
+	$address = $_POST['address'];
+	$profile = $_FILES['profile'];
+
+//-----------upload photo-----------//
+	$basepath = "photo/";
+	$fullpath = $basepath.$profile["name"];
+	move_uploaded_file($profile['tmp_name'], $fullpath);
+//---------------------------------//
+
+	$student = [
+
+		"name"=>$name,
+		"email"=>$email,
+		"gender"=>$gender,
+		"address"=>$address,
+		"profile"=>$fullpath
+
+	];
+
+	//print_r($student);
+
+//---------------------------------//
+
+	$stuJson = file_get_contents("student.json");
+	if (!$stuJson) {
+		# code...
+		$stuJson = "[]";
+	}
+
+	$data_arr = json_decode($stuJson,true);
+	array_push($data_arr, $student);
+
+	$jsonStr = json_encode($data_arr,JSON_PRETTY_PRINT);
+
+	file_put_contents("student.json", $jsonStr);
+
+	header("location:index.php");
+
+ ?>
